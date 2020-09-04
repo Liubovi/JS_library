05fe49849aee7cfb6afd394dc06c64239edad503
Liubovi <liubovi.t.l.a@gmail.com>
2020-09-04 17:27:37 +0300

    Получение эл-тов на странице

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -2,7 +2,9 @@ import $ from './core';
 import './modules/display';
 import './core';
 import './modules/classes';
+import './modules/handlers';
 import './modules/actions';
+
 
 
 export default $;
